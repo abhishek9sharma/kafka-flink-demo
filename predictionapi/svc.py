9396860bb2ae16318917ae6b71459d095e386fe0
abhishek9sharma <oldmonk@DESKTOP-MAKL8JG.localdomain>
2022-05-08 00:21:18 +0800

    remove unused code

--- predictionapi/svc.py
+++ predictionapi/svc.py
@@ -1,8 +1,4 @@
-# #UNCOMMENT BELOW 2 LINES IF RUNNING LOCALLY
-# from dotenv import load_dotenv
-# load_dotenv(dotenv_path = '.env')
 import os
-print(os.listdir('.'))
 from fastapi import FastAPI
 from predictionapi.routers import prediction_router
 
@@ -18,9 +14,3 @@ app.include_router(
     prediction_router.router,
     tags=['make predictions']
 )
-
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# def add(a,b):
